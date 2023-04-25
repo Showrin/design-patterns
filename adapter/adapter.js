19de386b091c364d60afe52aeac36d2406b11f9f
Showrin <showrinece15@gmail.com>
2023-04-26 02:39:44 +0600

    :sparkles: feat (adapter): Re-implement adapter design pattern in js

--- adapter/adapter.js
+++ adapter/adapter.js
@@ -1,52 +1,101 @@
-class PendriveAdapter {
-	convert_usb_a_to_usb_c(data) {
-		console.log("Converting USB A (male) to USB C (male) input...");
+class Serializer {
+  data = null;
 
-		if (data["usb_a"]) {
-			return {
-				usb_c: data["usb_a"],
-			};
-		}
+  constructor({ obj, data }) {
+    if (obj) {
+      this.data = this.convert_to_json(obj);
+    } else if (data) {
+      this.data = this.convert_to_object(data);
+    }
+  }
 
-		throw Error("Invalid data provided to the adapter.");
-	}
+  convert_to_json(obj) {
+    console.log("Converted JS Object to JSON.");
+    return `${obj} in json format`;
+  }
+
+  convert_to_object(data) {
+    console.log("Converted JSON to JS Object.");
+    return `${data} in object format`;
+  }
 }
 
-class Phone {
-	read_external_memory_data(data) {
-		if (data["usb_c"]) {
-			return data["usb_c"];
-		}
+class ApiView {
+  list(request) {
+    const data = request["data"];
 
-		throw Error("Invalid data provided to the phone.");
-	}
+    const serialized_request_data = new Serializer({ data }).data;
+    console.log(serialized_request_data);
+
+    console.log("-------------------------");
+    const excepted_result = "Expected result";
+
+    const serialized_response_data = new Serializer({ obj: excepted_result })
+      .data;
+    console.log(serialized_response_data);
+
+    return serialized_response_data;
+  }
 }
 
-class Pendrive {
-	constructor(data) {
-		this.data = data;
-	}
+list_api_view = new ApiView().list({ data: "Request body" });
 
-	get_data() {
-		console.log("Reading data from the pendrive...");
+// Output
+// Converted JSON to JS Object.
+// Request body in object format
+// -------------------------
+// Converted JS Object to JSON.
+// Expected result in json format
 
-		return {
-			usb_a: this.data,
-		};
-	}
-}
+// class PendriveAdapter {
+// 	convert_usb_a_to_usb_c(data) {
+// 		console.log("Converting USB A (male) to USB C (male) input...");
 
-const pendrive = new Pendrive("This is pendrive data");
-const pendrive_data = pendrive.get_data();
-console.log(`Pendrive data => ${JSON.stringify(pendrive_data)}\n....`);
+// 		if (data["usb_a"]) {
+// 			return {
+// 				usb_c: data["usb_a"],
+// 			};
+// 		}
 
-const adapter = new PendriveAdapter();
-const converted_data = adapter.convert_usb_a_to_usb_c(pendrive_data);
-console.log(`Adapter output data => ${JSON.stringify(converted_data)}\n....`);
+// 		throw Error("Invalid data provided to the adapter.");
+// 	}
+// }
 
-const phone = new Phone();
-const phone_data = phone.read_external_memory_data(converted_data);
-console.log(`Phone read data => ${phone_data}`);
+// class Phone {
+// 	read_external_memory_data(data) {
+// 		if (data["usb_c"]) {
+// 			return data["usb_c"];
+// 		}
+
+// 		throw Error("Invalid data provided to the phone.");
+// 	}
+// }
+
+// class Pendrive {
+// 	constructor(data) {
+// 		this.data = data;
+// 	}
+
+// 	get_data() {
+// 		console.log("Reading data from the pendrive...");
+
+// 		return {
+// 			usb_a: this.data,
+// 		};
+// 	}
+// }
+
+// const pendrive = new Pendrive("This is pendrive data");
+// const pendrive_data = pendrive.get_data();
+// console.log(`Pendrive data => ${JSON.stringify(pendrive_data)}\n....`);
+
+// const adapter = new PendriveAdapter();
+// const converted_data = adapter.convert_usb_a_to_usb_c(pendrive_data);
+// console.log(`Adapter output data => ${JSON.stringify(converted_data)}\n....`);
+
+// const phone = new Phone();
+// const phone_data = phone.read_external_memory_data(converted_data);
+// console.log(`Phone read data => ${phone_data}`);
 
 // Output:
 // Reading data from the pendrive...
